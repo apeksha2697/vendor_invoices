91ff1213931bc34dc7417b9dbd20fd511b8799f5
Apeksha Singh <apekshasingh664@gmail.com>
2020-10-31 13:58:53 +0530

    added unique index on vendors code

--- db/migrate/20201031071952_create_vendors.rb
+++ db/migrate/20201031071952_create_vendors.rb
@@ -1,9 +1,10 @@
 class CreateVendors < ActiveRecord::Migration[6.0]
   def change
     create_table :vendors do |t|
-      t.string :code, null: false, unique: true
+      t.string :code, null: false
       t.string :name, null: false
       t.string :category, null: false
+      t.index :code, unique: true
 
       t.timestamps
     end
