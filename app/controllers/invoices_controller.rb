4543df7e28cf628b2e661e10878949f5627dd522
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-01 15:39:30 +0530

    added actions to import files and read them

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,7 +1,21 @@
 class InvoicesController < ApplicationController
+  
   def index
-    @invoice = Invoice.all
+    @invoices = Invoice.all
   end
 
-  
+  def create
+    summary_data = Invoice.import(params["/invoices/import"]['file'])
+    redirect_to invoices_summary_path(summary_data)
+  end
+ 
+  def summary
+    @summary_data = params.permit(:amount, :invalid_invoices, :invoice_uploaded, :vendor_uploaded)
+  end
+ 
+  private
+
+    def invoice_params
+      params.require(:invoice).permit(:id, :invoice_number, :document_number, :document_type, :due_date, :document_date, :posting_date, :amount)
+    end
 end
