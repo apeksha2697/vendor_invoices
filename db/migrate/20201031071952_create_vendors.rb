9f3360da61d5d9ce8240093384b393db97a578ba
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-01 15:33:55 +0530

    changed vendor_type to string

--- db/migrate/20201031071952_create_vendors.rb
+++ db/migrate/20201031071952_create_vendors.rb
@@ -3,7 +3,7 @@ class CreateVendors < ActiveRecord::Migration[6.0]
     create_table :vendors do |t|
       t.string :code, null: false
       t.string :name, null: false
-      t.string :category, null: false
+      t.string :vendor_type, null: false
       t.index :code, unique: true
 
       t.timestamps
