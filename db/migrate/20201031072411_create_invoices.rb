f7c041aefa4e1671313e8df9e92b23685bdf6083
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-01 15:34:56 +0530

    updated columns of invoice

--- db/migrate/20201031072411_create_invoices.rb
+++ db/migrate/20201031072411_create_invoices.rb
@@ -1,15 +1,15 @@
 class CreateInvoices < ActiveRecord::Migration[6.0]
   def change
     create_table :invoices do |t|
-      t.integer :number, null: false
+      t.integer :invoice_number, null: false
       t.integer :document_number, null: false
       t.string :document_type, null: false
       t.date :due_date, null: false
       t.date :document_date, null: false
       t.date :posting_date, null: false
-      t.integer :amount, null: false
+      t.float :amount, null: false
       t.references :vendor, null: false, foreign_key: true
-      t.index :document_number, unique: true
+      t.index :invoice_number, unique: true
 
       t.timestamps
     end
