e4983db2ede6dfb107f8c3c9315377fea2f812dd
Apeksha Singh <apekshasingh664@gmail.com>
2020-11-01 15:31:48 +0530

    added validations for vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,9 @@
 class Vendor < ApplicationRecord
+  #associations
+  has_many :invoices
+
+  #validations
+  validates :code, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :vendor_type, presence: true
 end
